7a84f2cddcd80ad9496b9369415679e5065962cf
Michael <michael.allerby@designworks.co.nz>
2015-10-12 12:06:54 +1300

    layout/formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #return title foreach page
+    def full_title(page_title = '')
+        base_title = "Typebase"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
