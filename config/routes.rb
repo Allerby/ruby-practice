2dba149ce74e234ba7064f34ceeef57dea529a61
Michael <michael.allerby@designworks.co.nz>
2015-10-12 15:21:35 +1300

    add user controller and view, add test for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :articles
-  get 'signup' => 'users#new'
 
+  root 'static_pages#home'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
   get 'home/index'
+
+  resources :articles
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -10,8 +14,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  # You can have the root of your site routed with "root"
-  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
